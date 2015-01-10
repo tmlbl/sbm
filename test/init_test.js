edcafbef78b5b57c90e8371a835bb8be0c349a8f
Tim Lebel <tim.lebel@gmail.com>
2015-01-09 21:59:36 -0800

    improved init command

--- test/init_test.js
+++ test/init_test.js
@@ -1,27 +1,41 @@
 var init = require('../lib/init'),
     assert = require('chai').assert,
     exec = require('child_process').exec,
-    fs = require('fs');
+    fs = require('fs'),
+    setup = require('./setup');
 
 describe('Init command', function () {
 
-  it('Should not cause an error', function (next) {
-    init({}, function (err) {
-      assert.equal(err, null);
-      next();
-    });
-  });
-
-  it('Should create sbm.json', function (next) {
-    fs.readFile('./sbm.json', function (err, content) {
-      assert.lengthOf(content, 50, 'sbm.json has length of 50');
-      next();
-    });
-  });
-
-  after(function (done) {
+  afterEach(function (done) {
     exec('rm sbm.json', function () {
       done();
+    });
+  });
+
+  it('Should not cause an error', function (done) {
+    init({}, function (err) {
+      assert.equal(err, null);
+      done();
+    });
+  });
+
+  it('Should create sbm.json', function (done) {
+    init({}, function () {
+      fs.readFile('./sbm.json', function (err, content) {
+        assert.lengthOf(content, 50, 'sbm.json has length of 50');
+        done();
+      });
+    });
+  });
+
+  it('Should not overwrite existing file', function (done) {
+    exec('echo "11" > sbm.json', function () {
+      init({}, function () {
+        fs.readFile('./sbm.json', function (err, content) {
+            assert.lengthOf(content, 3, 'sbm.json has length of 3');
+            done();
+        });
+      });
     });
   });
 
